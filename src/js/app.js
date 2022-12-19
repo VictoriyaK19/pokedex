75b8f9a21e52ca31190c29bc998f7769ce8043da
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-12-19 17:45:09 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
+import { data } from "autoprefixer";
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+
   const ul = document.querySelector("ul");
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+
+    let result = data.results;
+    result.forEach(element => {
+
+
+      let li = document.createElement("li");
+      li.innerHTML = element.name;
+      ul.appendChild(li);
+      });
+    
+  });
 });
+
